f82d6e7e095e423875168f2d9bafdf8f43ef2079
Can Selcik <selcik.can@gmail.com>
2020-10-14 15:01:43 -0700

    checking in build artifacts

---
+++ build/precache-manifest.01e74289a64e36b7ee81058d7ed297fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8036d0356537dbed18611367fe52359",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ec80525eca2e5b91600",
+    "url": "/static/css/main.0ecd623c.chunk.css"
+  },
+  {
+    "revision": "c0b5eb78febb97e6e428",
+    "url": "/static/js/2.47dc701b.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/static/js/2.47dc701b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ec80525eca2e5b91600",
+    "url": "/static/js/main.6590d921.chunk.js"
+  },
+  {
+    "revision": "b7094251e8388f6546f1",
+    "url": "/static/js/runtime-main.ca639586.js"
+  }
+]);
